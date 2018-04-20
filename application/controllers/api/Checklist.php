819cf47ee0cd673f87afa406a265300d2c33ee06
Mhar <mhar0987@live.com>
2018-04-20 17:33:06 +0800

    ^^

--- application/controllers/api/Checklist.php
+++ application/controllers/api/Checklist.php
@@ -20,9 +20,11 @@ class Checklist extends CI_Controller {
 
 	public function get_my_checklist(){
 		$store_id = $this->post->store_id;
+		$user_id = $this->post->user_id;
 
-		$this->db->select("c.checklist_id , c.checklist_name");
-		$result = $this->db->where("c.store_id" , $store_id)->where("c.status" , 1)->where("c.deleted IS NULL")->get("checklist c")->result();
+		$this->db->select("c.checklist_id , c.checklist_name , c.vehicle_type");
+		$this->db->join("user_checklist uc" , "uc.checklist_id = c.checklist_id");
+		$result = $this->db->where("c.store_id" , $store_id)->where("uc.user_id" , $user_id)->where("c.status" , 1)->where("c.deleted IS NULL")->get("checklist c")->result();
 
 		echo json_encode(["status" => 1 , "data" => $result]);
 	}
@@ -50,8 +52,13 @@ class Checklist extends CI_Controller {
 
 		$result = $this->db->select()->where("checklist_id" , $checklist_id)->where("DELETED IS NULL")->order_by("item_position" , "ASC")->get("checklist_items")->result();
 
+		foreach($result as $key => $row){
+			$result[$key]->color = "light";
+		}
+
 		echo json_encode(["status" => true , "data" => $result]);
 	}
+
 	public function save_checklist(){
 		if($this->post){
 			$data = $this->post;
@@ -61,15 +68,17 @@ class Checklist extends CI_Controller {
 			//Create report
 			$this->db->insert("report" , [
 				"report_by"						=> $data->user->user_id ,
-				"vehicle_registration_number"	=> $data->vehicle_registration_number ,
-				"trailer_number"				=> $data->trailer_number ,
+				"vehicle_registration_number"	=> (isset($data->vehicle_registration_number)) ? $data->vehicle_registration_number : "" ,
+				"trailer_number"				=> (isset($data->trailer_number)) ? $data->trailer_number : "" ,
 				"checklist_id"					=> $data->checklist_type ,
 				"start_mileage"					=> $data->start_mileage ,
 				"end_mileage"					=> $data->end_mileage ,
-				"report_notes"					=> $data->note ,
+				"report_notes"					=> (isset($data->note)) ? $data->note : "" ,
 				"created"						=> time()
 			]);
 			$report_id = $this->db->insert_id();
+
+			$signature_path = $this->save_signature($report_id);
 
 			//Create status
 			$this->db->insert("report_status" , [
@@ -77,32 +86,38 @@ class Checklist extends CI_Controller {
 				"status"			=> ($this->isDefect()) ? 1 : 0 ,
 				"notes"				=> "Initial" ,
 				"user_id"			=> $data->user->user_id ,
-				"created"			=> time()
+				"created"			=> time(),
+				"longitude"			=> $data->longitude,
+				"latitude"			=> $data->latitude,
+				"signature"			=> $signature_path
 			]);
 			$status_id = $this->db->insert_id();
 
 			//save checklist items
 
-			$item_batch = array();
 			foreach($data->checklist as $item){
-				$item_batch[] = array(
+				$item_batch = array(
 					"report_id"				=> $report_id ,
 					"checklist_item_id"		=> $item->checklist_id ,
 					"checklist_value"		=> $item->note ,
-					"checklist_ischeck"		=> $item->checkbox
+					"checklist_ischeck"		=> $item->checkbox,
 				);
-			}
 
-			$this->db->insert_batch("report_checklist" , $item_batch);
+				$this->db->insert("report_checklist" , $item_batch);
+				$report_checklist_id = $this->db->insert_id();
+
+				if($item->images){
+					$this->save_image($report_id , $report_checklist_id , $item->images);
+				}
+			}
 
 			$report_number = date("dmY").'-'.sprintf('%05d', $report_id);
 
-			$signature_path = $this->save_signature($report_number);
+			
 
 			$this->db->where("report_id" , $report_id)->update("report" , [
 				"report_number"		=> $report_number,
-				"status_id"			=> $status_id,
-				"signature"			=> $signature_path
+				"status_id"			=> $status_id 
 			]);
 
 			$this->db->trans_complete();
@@ -117,6 +132,7 @@ class Checklist extends CI_Controller {
 	        }
 		}
 	}
+
 	private function return_false($rules = 0){
 		$msg = "Server Error , Please Try Again Later";
 
@@ -184,5 +200,55 @@ class Checklist extends CI_Controller {
 	    file_put_contents($path, $data);
 
         return $year."/".$month.'/'.$name;
+	}
+
+	private function save_image($report_id , $report_checklist_id , $images){
+
+		$img_batch = array();
+
+		foreach($images as $img){
+
+			$name = $report_id.'_'.$report_checklist_id.'_'.time().'.PNG';
+	        $year = date("Y");
+	        $month = date("m");
+	        
+	        $folder = "./public/upload/report/".$year."/".$month;
+	        
+	        $date = time();
+
+	        if (!file_exists($folder)) {
+	            mkdir($folder, 0777, true);
+	            mkdir($folder.'/thumbnail', 0777, true);
+	            create_index_html($folder);
+	        }
+
+
+	        $path = $folder.'/'.$name;
+
+	        $encoded = $img;
+
+		    //explode at ',' - the last part should be the encoded image now
+		    $exp = explode(',', $encoded);
+
+		    //we just get the last element with array_pop
+		    $base64 = array_pop($exp);
+
+		    //decode the image and finally save it
+		    $data = base64_decode($base64);
+
+
+		    //make sure you are the owner and have the rights to write content
+		    file_put_contents($path, $data);
+
+		    $img_batch[] = array(
+		    	"report_id"				=> $report_id ,
+		    	"report_checklist_id"	=> $report_checklist_id ,
+		    	"image_path"			=> $year."/".$month.'/' ,
+		    	"image_name"			=> $name
+		    );
+		}
+
+		$this->db->insert_batch('report_images', $img_batch);
+
 	}
 }
